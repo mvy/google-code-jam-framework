7d3a1eb991e6678e8227ea7778688a0458ad5843
Yves "M'vy" Stadler <mvyonline@gmail.com>
2018-04-06 13:27:03 +0200

    Add package distribution files

---
+++ setup.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(name='GCJCore',
+      version='1.0',
+      description='AppState reader for python scripts',
+      author='Yves Stadler',
+      author_email='yves.stadler@gmail.com',
+      url='',
+      packages=['gcjcore'],
+     )
